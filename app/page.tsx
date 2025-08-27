4d518c5caed30995b342c69aeaecac0296f57864
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-27 07:34:58 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Layout/Header"
+import Footer from "@/components/Layout/Footer"
+// ...rest as-is
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  )
+}
