8d72e742c9fe586c506db907570d993215e32033
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-27 07:27:08 +0100

    Create page.tsx

---
+++ app/terms/page.tsx
@@ -0,0 +1,9 @@
+import LegalLayout from "@/components/Layout/LegalLayout"
+
+export default function Page() {
+  return (
+    <LegalLayout title="Terms of Service">
+      <p>Use at your own discretion. By deploying a portfolio, you agree it will be public via GitHub Pages unless you delete it or it expires.</p>
+    </LegalLayout>
+  )
+}
