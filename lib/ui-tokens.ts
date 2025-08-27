12a7efc78b17a00bbe7def84d35e7e8041b12b09
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-27 07:26:02 +0100

    Create ui-tokens.ts

---
+++ lib/ui-tokens.ts
@@ -0,0 +1,20 @@
+export const themes = {
+  blue: {
+    primary: "text-blue-600 dark:text-blue-400",
+    bullet: "text-blue-600 dark:text-blue-400",
+  },
+  emerald: {
+    primary: "text-emerald-600 dark:text-emerald-400",
+    bullet: "text-emerald-600 dark:text-emerald-400",
+  },
+  rose: {
+    primary: "text-rose-600 dark:text-rose-400",
+    bullet: "text-rose-600 dark:text-rose-400",
+  },
+} as const
+
+export const fonts = {
+  inter: "font-sans",
+  lora: "font-serif",
+  roboto: "font-serif",
+} as const
