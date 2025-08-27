814c3d3475718467396e5f4bd34e3754db3f1d92
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-08-27 07:21:06 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true }, // allows base64 images for headshots in preview
+}
+
+export default nextConfig
